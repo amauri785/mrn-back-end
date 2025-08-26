c4a3d84f7c0572ecc7308422bbc562a7aad60313
amauri785 <abrahamtokpah785@gmail.com>
2025-08-26 13:10:05 +0200

    uuu

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import bookRoutes from './routes/bookRoutes.js'
+import cors from 'cors'
 import authRoutes from './routes/userRoutes.js'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
